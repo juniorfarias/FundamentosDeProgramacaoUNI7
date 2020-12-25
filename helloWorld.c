b97cd0fd728bedf2a8586815c2ac2d3a73e176c7
repl.it user <replituser@example.com>
2020-12-25 17:39:45 +0000

    First commit Fundamentos de Programação

---
+++ helloWorld.c
@@ -0,0 +1,13 @@
+/*
+** Autor: juniorfarias
+** Data: 25/12/2020
+** Objetivo: Configuração do ambiente e imprimir um Hello World
+*/
+#include <stdio.h>
+int main(void) {
+  
+  //Sai zika
+  printf("Hello World!!!\n");
+
+  return 0;
+}
